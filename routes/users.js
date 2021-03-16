9dcfe15aacff657398d7a72ca1e1d35329c16216
johnson <phantruongson96@gmail.com>
2021-03-16 17:42:59 -0400

    final

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
+let users = [
   {
     firstName: "John",
     lastName: "Doe",
@@ -17,13 +18,7 @@ const users = [
     firstName: "John",
     lastName: "Smith",
     age: 29
-  },
-  {
-    firstName: "Johnson",
-    lastName: "Phan",
-    age: 24
-  },
-
+  }
 ]
 
 router.get('/', (req, res) => {
@@ -32,9 +27,46 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  users.push(req.body);
-  console.log(users);
+  const user = req.body;
+
+  users.push({ ...user, id: uuidv4() });
+
   res.send(req.body);
-})
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const user = users.find((user) => user.id === id );
+  res.send(user);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  
+  users = users.filter(user => user.id !== id );
+  res.send(users)
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = user.find((user) => user.id = id );
+
+  if(firstName) {
+    user.firstName = firstName;
+  }
+
+  if(lastName) {
+    user.lastName = lastName;
+  }
+
+  if(age) {
+    user.age = age;
+  }
+
+  res.send('User has been updated');
+})  
+
 
 export default router;
